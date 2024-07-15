223aa073b6fd06dd2d31dee6173c5d25fdec51b5
ionut8891 <148498333+ionut8891@users.noreply.github.com>
2024-07-15 10:49:39 -0400

    Exercice_2_Requis_5_refactoring

--- greeting.js
+++ greeting.js
@@ -1,14 +1,19 @@
 function greet(name) {
+  if (isGreetingForMultipleNames(name)) {
+    return `Hello, ${name.slice(0, -1).join(', ')}, and ${name.slice(-1)}!`;
+  }
   if(Array.isArray(name) && name.length === 2)
     return `Hello, ${name[0]} and ${name[1]}!`;
-  else if (Array.isArray(name) && name.length > 2)
-    return `Hello, ${name.slice(0, -1).join(', ')}, and ${name.slice(-1)}!`;
   else if (name === undefined || name === null || name === "")
     return 'Hello there!';
   else if(name===name.toUpperCase())
     return `HELLO ${name}!`;
   else
   return `Hello, ${name}!`;
+
+}
+function isGreetingForMultipleNames(name) {
+  return Array.isArray(name) && name.length > 2;
 }
 
 module.exports = greet;
